bcdaaaad910e53db713e428c423561097028ecbc
tickhcj <307143503@qq.com>
2017-07-22 07:24:33 +0800

    num add count

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# What to do
+print "I will now count my chickens:"
+
+# The number of chickens
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100 - 25.0 * 3.0 % 4.0
+
+# cout the eggs
+print "Now I will count the eggs:"
+
+# The calculation results
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# To determine the size
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# To compare
+print 3 + 2 < 5 - 7
+
+# Comparing the two groups of Numbers
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# The results of
+print "Oh, that's why it's False."
+
+# How about some more
+print "How about some more,"
+
+# The comparison of more
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
