b5476c29d3730199629ffaea474921513eca0915
tickhcj <307143503@qq.com>
2017-07-22 23:37:44 +0800

    add ex6-11

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?(centimeter)",
+height = raw_input()
+print "How much do you weight?(kg)",
+weight = raw_input()
+
+print "So, you're %r old, %rcen tall and %rkg heavy." % (
+	age, height, weight)
